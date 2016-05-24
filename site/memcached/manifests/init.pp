c6c67ac649ae15b0489947db9cd3fce6c14efdc6
moorea7 <moorea7@users.noreply.github.com>
2016-05-24 14:16:02 -0500

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,20 @@
+class memcached {
+  package { 'memcached' : 
+    ensure => installed,
+  }
+  
+  file { 'memcached_config' :
+    ensure  => file,
+    mode    => '0644',
+    user    => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/memcached/memcached.config',
+    require => Package['memcached'],
+  }
+  
+  service { 'memcached' :
+    ensure    => running,
+    enable    => true,
+    subscribe => File['memcached_config'],
+  }
+    
