f6a789cba450a3f3101f97a0991c5f8dcf635d87
moorea7 <moorea7@users.noreply.github.com>
2016-05-24 16:17:02 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -10,6 +10,37 @@ class nginx {
     skip_if_unavailable => '1',
   }
   
+  yumrepo { 'updates':
+    ensure              => 'present',
+    descr               => 'CentOS-$releasever - Updates',
+    enabled             => '1',
+    gpgcheck            => '1',
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+    priority            => '99',
+    skip_if_unavailable => '1',
+  }
+  
+  yumrepo { 'extras':
+    ensure              => 'present',
+    descr               => 'CentOS-$releasever - Extras',
+    enabled             => '1',
+    gpgcheck            => '1',
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
+    priority            => '99',
+    skip_if_unavailable => '1',
+  }
+  
+  yumrepo { 'centosplus':
+    ensure     => 'present',
+    descr      => 'CentOS-$releasever - Plus',
+    enabled    => '1',
+    gpgcheck   => '1',
+    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
+  }
+  
   package { 'nginx' :
     ensure  => present,
     require => Yumrepo['base'],
