87a5ad852db69d4c1f126511eb95191d958b1559
Anthony <anthony@love-coffee.com>
2016-05-25 20:26:41 +0000

    Platform Abstraction Lab

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,68 +1,100 @@
 class nginx {
+
+  case $::osfamily {
+    'redhat', 'debian' : {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $configdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+      $svrblockdir = '/etc/nginx/conf.d'
+    }
+    'windows' : {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $configdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
+      $svrblockdir = 'C:/ProgramData/nginx/conf.d'
+    }
+    default : {
+      fail("Module ${module_name} is not intended to run on ${::osfamily}")
+    }
+  }
+
+  $runasuser = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
+
   File {
     mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => Package[$module_name],
+    owner   => $owner,
+    group   => $group,
+    require => Package[$package],
   }
 
-  Yumrepo {
-    ensure              => present,
-    enabled             => '1',
-    gpgcheck            => '1',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    before              => Package[$module_name],
-  }
+  if $::osfamily == 'RedHat' {
+    Yumrepo {
+      ensure              => present,
+      enabled             => '1',
+      gpgcheck            => '1',
+      priority            => '99',
+      skip_if_unavailable => '1',
+      gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+      before              => Package[$package],
+    }
 
-  yumrepo { 'base':
-    descr      => 'CentOS-$releasever - Base',
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
-  }
+    yumrepo { 'base':
+      descr      => 'CentOS-$releasever - Base',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
+    }
 
-  yumrepo { 'updates':
-    descr      => 'CentOS-$releasever - Updates',
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-  }
+    yumrepo { 'updates':
+      descr      => 'CentOS-$releasever - Updates',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+    }
 
-  yumrepo { 'extras':
-    descr      => 'CentOS-$releasever - Extras',
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
-  }
+    yumrepo { 'extras':
+      descr      => 'CentOS-$releasever - Extras',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
+    }
 
-  yumrepo { 'centosplus':
-    descr      => 'CentOS-$releasever - Plus',
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
+    yumrepo { 'centosplus':
+      descr      => 'CentOS-$releasever - Plus',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
+    }
   }
-
-  package { $module_name :
+  package { $package :
     ensure  => present,
   }
 
-  file { ['/var/www', '/etc/nginx', '/etc/nginx/conf.d'] :
+  file { [$docroot, $configdir, $svrblockdir] :
     ensure  => directory,
   }
 
   file { 'configfile' :
-  	ensure  => file,
-  	path    => '/etc/nginx/nginx.conf',
-  	source  => 'puppet:///modules/nginx/nginx.conf',
+    ensure  => file,
+    path    => "${configdir}/nginx.conf",
+    content  => template('nginx/nginx.conf.erb'),
   }
 
   file { 'serverblock' :
     ensure  => file,
-    path    => '/etc/nginx/conf.d/default.conf',
-    source  => 'puppet:///modules/nginx/default.conf',
+    path    => "${svrblockdir}/default.conf",
+    content  => template('nginx/default.conf.erb'),
   }
 
   file { 'index.html' :
     ensure  => file,
-    path    => '/var/www/index.html',
+    path    => "${docroot}/index.html",
     source  => 'puppet:///modules/nginx/index.html',
   }
 
-  service { $module_name :
+  service { 'nginx' :
     ensure    => running,
     enable    => true,
     subscribe => File['configfile', 'serverblock'],
