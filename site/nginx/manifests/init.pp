56131eecbbe04162d24b1e8f2de85ce441b939cf
moorea7 <moorea7@users.noreply.github.com>
2016-05-25 09:15:28 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -8,6 +8,7 @@ class nginx {
     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
     priority            => '99',
     skip_if_unavailable => '1',
+    before              => Package['nginx'],
   }
   
   yumrepo { 'updates':
@@ -19,6 +20,7 @@ class nginx {
     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
     priority            => '99',
     skip_if_unavailable => '1',
+    before              => Package['nginx']	,
   }
   
   yumrepo { 'extras':
@@ -30,6 +32,7 @@ class nginx {
     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
     priority            => '99',
     skip_if_unavailable => '1',
+    before              => Package['nginx'],
   }
   
   yumrepo { 'centosplus':
@@ -39,10 +42,53 @@ class nginx {
     gpgcheck   => '1',
     gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
+    before     => Package['nginx'],
   }
   
   package { 'nginx' :
     ensure  => present,
-    require => Yumrepo['base'],
+  }
+  
+  file { 'docroot' :
+    ensure => directory,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+    path   => '/var/www',
+  }
+  
+  file { 'configfile' :
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    path    => '/etc/nginx/nginx.conf',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  
+  file { 'serverblock' :
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    path    => '/etc/nginx/conf.d/default.conf',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  
+  file { 'index.html' :
+    ensure  => file,
+    path    => '/var/www/index.html',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/nginx/index.html',
+  }
+  
+  service { 'nginx':
+	  ensure    => running,
+	  enable    => true,
+	  subscribe => File['configfile', 'serverblock', 'index.html']
   }
 }
