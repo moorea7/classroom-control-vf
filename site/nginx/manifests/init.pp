f203e5f44e58ed9e7b2d138d7cb064d832253b5d
Anthony <anthony@love-coffee.com>
2016-05-25 16:19:54 +0000

    fixing formatting

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,11 +1,11 @@
 class nginx {
-  File { 
-	mode    => '0644',
-	owner   => 'root',
-	group   => 'root',
-	require => Package[$module_name],
+  File {
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => Package[$module_name],
   }
-  
+
   Yumrepo {
     ensure              => present,
     enabled             => '1',
@@ -20,22 +20,22 @@ class nginx {
     descr      => 'CentOS-$releasever - Base',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
   }
-  
+
   yumrepo { 'updates':
     descr      => 'CentOS-$releasever - Updates',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
   }
-  
+
   yumrepo { 'extras':
     descr      => 'CentOS-$releasever - Extras',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
   }
-  
+
   yumrepo { 'centosplus':
     descr      => 'CentOS-$releasever - Plus',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
   }
-  
+
   package { $module_name :
     ensure  => present,
   }
@@ -43,25 +43,25 @@ class nginx {
   file { ['/var/www', '/etc/nginx', '/etc/nginx/conf.d'] :
     ensure  => directory,
   }
-  
+
   file { 'configfile' :
   	ensure  => file,
   	path    => '/etc/nginx/nginx.conf',
   	source  => 'puppet:///modules/nginx/nginx.conf',
   }
-  
+
   file { 'serverblock' :
     ensure  => file,
     path    => '/etc/nginx/conf.d/default.conf',
     source  => 'puppet:///modules/nginx/default.conf',
   }
-  
+
   file { 'index.html' :
     ensure  => file,
     path    => '/var/www/index.html',
     source  => 'puppet:///modules/nginx/index.html',
   }
-  
+
   service { $module_name :
     ensure    => running,
     enable    => true,
