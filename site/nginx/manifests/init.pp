47d1858efb0441d0f7555bad46d6a71d7fcd33bf
moorea7 <moorea7@users.noreply.github.com>
2016-05-25 10:59:32 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,111 +1,71 @@
 class nginx {
-  yumrepo { 'base':
-    ensure              => 'present',
-    descr               => 'CentOS-$releasever - Base',
+  File { 
+	mode    => '0644',
+	owner   => 'root',
+	group   => 'root',
+	require => Package['nginx'],
+  }
+  
+  Yumrepo {
+    ensure              => present,
     enabled             => '1',
     gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
     priority            => '99',
     skip_if_unavailable => '1',
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     before              => Package['nginx'],
+  }
+
+  yumrepo { 'base':
+    descr      => 'CentOS-$releasever - Base',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
   }
   
   yumrepo { 'updates':
-    ensure              => 'present',
-    descr               => 'CentOS-$releasever - Updates',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before              => Package['nginx']	,
+    descr      => 'CentOS-$releasever - Updates',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
   }
   
   yumrepo { 'extras':
-    ensure              => 'present',
-    descr               => 'CentOS-$releasever - Extras',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before              => Package['nginx'],
+    descr      => 'CentOS-$releasever - Extras',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
   }
   
   yumrepo { 'centosplus':
-    ensure     => 'present',
     descr      => 'CentOS-$releasever - Plus',
-    enabled    => '1',
-    gpgcheck   => '1',
-    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
-    before     => Package['nginx'],
   }
   
   package { 'nginx' :
     ensure  => present,
   }
-  
-  file { 'docroot' :
-    ensure => directory,
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
-    path   => '/var/www',
-  }
-  
-  file { '/etc/nginx' :
+
+  file { ['/var/www', '/etc/nginx', '/etc/nginx/conf.d'] :
     ensure  => directory,
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['nginx'],
-  }
-  
-  file { '/etc/nginx/conf.d' :
-    ensure  => directory,
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['nginx'],
   }
   
   file { 'configfile' :
-    ensure  => file,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    path    => '/etc/nginx/nginx.conf',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
+  	ensure  => file,
+  	path    => '/etc/nginx/nginx.conf',
+  	source  => 'puppet:///modules/nginx/nginx.conf',
   }
   
   file { 'serverblock' :
     ensure  => file,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
     path    => '/etc/nginx/conf.d/default.conf',
     source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
   }
   
   file { 'index.html' :
     ensure  => file,
     path    => '/var/www/index.html',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
     source  => 'puppet:///modules/nginx/index.html',
   }
   
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    subscribe => File['configfile', 'serverblock', 'index.html'],
-    require   => Package['nginx'],
+    subscribe => File['configfile', 'serverblock'],
+    require   => File['index.html'],
   }
 }
