e3b2e97b42be3a392236df8f6598cd5b204ff627
Anthony <anthony@love-coffee.com>
2016-05-26 19:51:28 +0000

    removed extras

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,7 +1,4 @@
 class profile::apache {
   class { 'apache' : }
-  class { 'apache::mod::auth_basic' : }
-  class { 'apache::mod::rewrite' : }
   class { 'apache::mod::php' : }
-  class { 'apache::mod::mpm_module' : }
 }
