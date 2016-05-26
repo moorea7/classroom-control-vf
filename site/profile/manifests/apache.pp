f1637c4a7c21cfa6c9f43bf3ec1009a9f69ca5a7
Anthony <anthony@love-coffee.com>
2016-05-26 19:47:35 +0000

    More modules

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,3 +1,8 @@
 class profile::apache {
   class { 'apache' : }
+  class { 'apache::mod::rewrite' : }
+  class { 'apache::mod::auth_basic' : }
+  class { 'apache::mod::rewrite' : }
+  class { 'apache::mod::php' : }
+  class { 'apache::mod::mpm_module' : }
 }
