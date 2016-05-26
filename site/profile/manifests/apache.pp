0f77cfeb1bc0e06f1062140ad6b201c0906b54b8
Anthony <anthony@love-coffee.com>
2016-05-26 19:49:45 +0000

    removed extra mod_rewrite

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,6 +1,5 @@
 class profile::apache {
   class { 'apache' : }
-  class { 'apache::mod::rewrite' : }
   class { 'apache::mod::auth_basic' : }
   class { 'apache::mod::rewrite' : }
   class { 'apache::mod::php' : }
